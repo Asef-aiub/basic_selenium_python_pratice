5fcd8306f0c92a1aaf2023ef567354f1b0e61938
Asef-aiub <87447084+Asef-aiub@users.noreply.github.com>
2023-06-20 04:12:42 +0600

    basic practice

--- LearningSelenium/Demo_Auto_Suggest.py
+++ LearningSelenium/Demo_Auto_Suggest.py
@@ -1,5 +1,6 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 class DemoAutoSuggest():
@@ -7,24 +8,25 @@ class DemoAutoSuggest():
         driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
         driver.get("https://www.yatra.com/")
         driver.maximize_window()
-        driver.find_element(By.XPATH,"//span[@class='demo-icon icon-hotels']").click()
+        dpart= driver.find_element(By.XPATH, "//input[@id='BE_flight_origin_city']")
+        dpart.click()
         time.sleep(2)
-        driver.find_element(By.XPATH,"//span[@class='txt-ellipses hotel_passengerBox travellerPaxBox']").click()
+        dpart.send_keys("New Delhi")
         time.sleep(2)
-        driver.find_element(By.XPATH,"//div[@class='hotel_passengerBox dflex relative']//div[3]//div[1]//div[1]//span[2]").click()
+        dpart.send_keys(Keys.ENTER)
         time.sleep(2)
-        elem=driver.find_element(By.XPATH,"//select[@class='ageselect']").is_displayed()
-        print(elem)
-        time.sleep(2)
-        driver.find_element(By.XPATH,"//div[@class='hotel_passengerBox dflex relative']//div[3]//div[1]//div[1]//span[1]").click()
-        time.sleep(2)
-        try:
-            elem1 = driver.find_element(By.XPATH, "//select[@class='ageselect']").is_displayed()
-            print(elem1)
-        except:
-            print("Element not displayed")
-
+        arrival=driver.find_element(By.XPATH, "//input[@id='BE_flight_arrival_city']")
+        arrival.send_keys("New")
         time.sleep(4)
+        search=driver.find_elements(By.XPATH,"//div[@class='viewport']//div[1]/li")
+        print(len(search))
+        for i in search:
+            print(i.text)
+            if "New York (LGA)" in i.text:
+                i.click()
+                time.sleep(4)
+                break
+
 
 getAutoSuggest=DemoAutoSuggest()
 getAutoSuggest.locate()
