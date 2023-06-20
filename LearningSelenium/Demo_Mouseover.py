b51f0fd0ca2424af915756f9c01ef97c11804f60
Asef-aiub <87447084+Asef-aiub@users.noreply.github.com>
2023-06-21 04:02:59 +0600

    basic practice

--- LearningSelenium/Demo_Mouseover.py
+++ LearningSelenium/Demo_Mouseover.py
@@ -2,6 +2,8 @@ import time
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 class DemoMouseOver():
@@ -13,7 +15,13 @@ class DemoMouseOver():
         achains=ActionChains(driver)
         morebutton=driver.find_element(By.XPATH,"//span[@class='more-arr']")
         achains.move_to_element(morebutton).perform()
-        time.sleep(3)
+        #right click
+        #achains.context_click(morebutton).perform()
+        #double click
+        #achains.double_click(morebutton).perform()
+        # time.sleep(3)
+        wait=WebDriverWait(driver,10)
+        wait.until(EC.element_to_be_clickable((By.XPATH,"//span[normalize-space()='Adventures']")))
         driver.find_element(By.XPATH,"//span[normalize-space()='Adventures']").click()
         time.sleep(3)
 dmover=DemoMouseOver()
