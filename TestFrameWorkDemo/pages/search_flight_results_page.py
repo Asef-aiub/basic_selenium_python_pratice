2d2aab03d06d91595032d9e7bf4323006180c7f5
Asef-aiub <87447084+Asef-aiub@users.noreply.github.com>
2023-07-27 01:15:05 +0600

    testFramework

---
+++ TestFrameWorkDemo/pages/search_flight_results_page.py
@@ -0,0 +1,35 @@
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+
+class launchPage():
+    def __init__(self, driver,wait):
+        self.driver = driver
+        self.wait = wait
+    def departfrom(self,departlocation):
+        departfrom=self.wait.until(EC.element_to_be_clickable((By.XPATH,"//input[@id='BE_flight_origin_city']")))
+        departfrom.click()
+        departfrom.send_keys(departlocation)
+        departfrom.send_keys(Keys.ENTER)
+    def destination(self,destinationlocation):
+        destination=self.wait.until(EC.element_to_be_clickable((By.XPATH,"//input[@id='BE_flight_arrival_city']")))
+        destination.click()
+        destination.send_keys(destinationlocation)
+        #search_results=self.wait.until(EC.presence_of_element_located((By.XPATH,"//div[@class='viewport']//div[1]/li")))
+        destination.send_keys(Keys.ENTER)
+    def departuredate(self, departdate):
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@id='BE_flight_origin_date']"))).click()
+        dates_element = self.wait.until(
+            EC.element_to_be_clickable((By.XPATH, "//div[@id='monthWrapper']//tbody//td[@class!='inActiveTD weekend']"))
+        )
+        dates = dates_element.find_elements(By.XPATH, "//div[@id='monthWrapper']//tbody//td[@class!='inActiveTD weekend']")
+        for date in dates:
+            if date.get_attribute("data-date") == departdate:
+                date.click()
+                break
+
+    def search(self):
+        search=self.wait.until(EC.element_to_be_clickable((By.XPATH,"//input[@value='Search Flights']")))
+        search.click()
+        time.sleep(3)
---
+++ TestFrameWorkDemo/testcases/test_searchflights.py
@@ -0,0 +1,19 @@
+import pytest
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from pages.search_flight_results_page import launchPage
+
+
+@pytest.mark.usefixtures("setup")
+class TestSearchAndVerifyFilter():
+    def testSearchFlights(self):
+        lp=launchPage(self.driver,self.wait)
+        lp.departfrom("New Delhi")
+        time.sleep(3)
+        lp.destination("New York")
+        time.sleep(3)
+        lp.departuredate("21/08/2023")
+        time.sleep(3)
+        lp.search()
+        time.sleep(3)
